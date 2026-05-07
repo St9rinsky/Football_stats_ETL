2f7843ae07b42b7a7d152b634dd58ac515b23348
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-05-07 14:31:46 +0200

    modified the extraction to accomodate results and fixtures as separate features

--- extract/extract_football_api.py
+++ extract/extract_football_api.py
@@ -1,7 +1,7 @@
 import os
 import json
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,25 +13,17 @@ COMPETITION_CODE = "PL"
 COMPETITION_NAME = "premier_league"
 SEASON = 2025
 
-def save_json_to_bronze(data, dataset_name, competition_name, season):
-    now = datetime.now()
-    year = now.year
-    week = now.isocalendar().week
-    date = now.strftime("%Y-%m-%d")
+def save_json_to_bronze(data: json, dataset_name: str, competition_name: str, season: int):
+    """
+    Saving the Json into the correct folder\n
+    Saving the Json folder with the proper name\n
+    """
 
-    folder_path = (
-        f"data/bronze/football_api/"
-        f"competition={competition_name}/"
-        f"season={season}/"
-        f"year={year}/"
-        f"week={week}/"
-        f"{dataset_name}"
-    )
+    date = datetime.now().strftime("%Y-%m-%d")
+
+    folder_path = (f"data/bronze/{competition_name}/{season}/{dataset_name}")
     
-    file_name = (
-        f"{competition_name}_{dataset_name}_"
-        f"{season}_season_week_{week}_{date}.json"
-    )
+    file_name = (f"{competition_name}_{dataset_name}_on_{date}.json")
 
     file_path = f"{folder_path}/{file_name}"
 
@@ -40,17 +32,20 @@ def save_json_to_bronze(data, dataset_name, competition_name, season):
     with open(file_path, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4)
 
-    print(f"Saved bronze file: {file_path}\n")
+    print(f"Saved bronze {dataset_name} file: {file_path}\n")
 
+def fetch_from_api(endpoint: str, params=None):
+    """
+    Fetches data from the football api\n
+    Takes a url endpoint and a parameter\n\n
 
+    RETURNS: a JSON data object\n
+    RAISES: http error if occurs
 
-def fetch_from_api(endpoint, params=None):
-    headers = {
-        "X-Auth-Token": API_KEY
-    }
+    """
 
+    headers = {"X-Auth-Token": API_KEY}
     url = f"{BASE_URL}/{endpoint}"
-
     response = requests.get(url, headers=headers, params=params)
 
     print("Request URL:", response.url)
@@ -59,26 +54,36 @@ def fetch_from_api(endpoint, params=None):
     response.raise_for_status()
     return response.json()
 
-
-def extract_results():
+#http://api.football-data.org/v4/competitions/2003/matches?matchday=1"
+def extract_all_matches():
     data = fetch_from_api(
         endpoint=f"competitions/{COMPETITION_CODE}/matches",
-        params={
-            "season": SEASON
-        }
-    )
+        params={"season": SEASON}
+        )
 
-    save_json_to_bronze(data, "results", COMPETITION_NAME, SEASON)
+    save_json_to_bronze(data, "Matches", COMPETITION_NAME, SEASON)
 
+def extract_match_data(status :str, dataset_name :str):
+    date = datetime.now()
+    date_from = date.strftime("%Y-%m-%d")
+    date_to = date + timedelta(days = 7)
 
-def extract_standings():
     data = fetch_from_api(
-        endpoint=f"competitions/{COMPETITION_CODE}/standings"
-    )
+        endpoint=f"competitions/{COMPETITION_CODE}/matches",
+        params={"dateFrom":date_from,"dateTo":date_to.strftime("%Y-%m-%d"),"status":status}
+        )
+    
+    save_json_to_bronze(data, dataset_name,COMPETITION_NAME,SEASON)
 
-    save_json_to_bronze(data, "standings", COMPETITION_NAME, SEASON)
+# http://api.football-data.org/v4/competitions/PL/standings
+def extract_standings():
+    data = fetch_from_api( endpoint=f"competitions/{COMPETITION_CODE}/standings")
+
+    save_json_to_bronze(data, "Standings", COMPETITION_NAME, SEASON)
 
 
 if __name__ == "__main__":
-    extract_results()
+    extract_all_matches()
+    extract_match_data("SCHEDULED", "Fixtures")
+    extract_match_data("FINISHED", "Results")
     extract_standings()
